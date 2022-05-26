87f06b6d3c584ce37430db771fa000175cd14d95
wassai625 <98636488+wassai625@users.noreply.github.com>
2022-05-26 18:26:54 +0900

    feature-add-firestore

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -20,7 +20,7 @@ function sidebar() {
       <TwitterIcon className='sidebar_twitterIcon'/>
         
       {/* SidebarOption */}
-      <SidebarOption text="ホーム" Icon={HomeIcon}/>
+      <SidebarOption text="ホーム" Icon={HomeIcon} active />
       <SidebarOption text="話題を検索" Icon={SearchIcon}/>
       <SidebarOption text="通知" Icon={NotificationsNoneIcon}/>
       <SidebarOption text="メッセージ" Icon={MailOutlineIcon}/>
--- src/components/timeline/TweetBox.js
+++ src/components/timeline/TweetBox.js
@@ -1,24 +1,55 @@
 import { Button } from '@material-ui/core';
 import { Avatar } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import "./TweetBox.css";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore"; 
+import db from '../../firebase';
 
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    /* firebaseのデータベースに追加する */
+    e.preventDefault();
+
+    addDoc(collection(db, "posts"),{
+        displayName: "初ツイート",
+        username: "harapeko_wassai",
+        verified: true,
+        text: tweetMessage,
+        avatar: "http://shincode.info/wp-content/uploads/2021/12/icon.png",
+        image: tweetImage,
+        timestamp: serverTimestamp(),
+    });
+
+    setTweetImage("");
+    setTweetMessage("");
+  };
+
   return (
     <div className='tweetBox'>
       <form>
           <div className='tweetBox_input'>
           <Avatar />
-           <input placeholder='いまどうしてる？' 
-            type="text"></input>
+           <input
+           value={tweetMessage} 
+           placeholder='いまどうしてる？' type="text"
+           onChange={(e) => setTweetMessage(e.target.value)}
+           ></input>
           </div>
-          <input className='tweetBox_imageInput' placeholder='画像のURLを入力してください'
-           type="text"></input>
-          <Button className='tweetBox_tweetButton' 
-           type='submit'>ツイートする</Button>
+          <input 
+           value={tweetImage} 
+           className='tweetBox_imageInput' placeholder='画像のURLを入力してください'
+           type="text"
+           onChange={(e) => setTweetImage(e.target.value)}
+           ></input>
+          <Button className='tweetBox_tweetButton' type='submit'
+           onClick={sendTweet}
+          >ツイートする</Button>
       </form>
     </div>
-  );
+  )
 }
 
 export default TweetBox;
